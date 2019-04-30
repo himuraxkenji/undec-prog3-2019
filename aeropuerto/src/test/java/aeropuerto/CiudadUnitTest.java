e26ea15ebd85c0a40ab5ec09cc844472edca7056
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-04-30 13:30:18 -0300

    Creación Test de Ciudad

---
+++ aeropuerto/src/test/java/aeropuerto/CiudadUnitTest.java
@@ -0,0 +1,43 @@
+package aeropuerto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CiudadUnitTest {
+
+	public void instanciarCiudad_TodosLosAtributos_InstanciaCorrecta() {
+		//arrange y act
+		Ciudad unaCiudad = new Ciudad(1, "Chilecito", "5360" );
+		
+		//assert
+		assertEquals("Chilecito", unaCiudad.getNombre());
+		assertEquals("5360", unaCiudad.getCodigoPostal());
+	}
+	
+	public void instanciarCiudad_FaltaCodigoPostal_InstanciaCorrecta() {
+		//arrange y act
+		Ciudad unaCiudad = new Ciudad(1, "Chilecito", "");
+		
+		//assert
+		assertEquals("Chilecito", unaCiudad.getNombre());
+		assertEquals("", unaCiudad.getCodigoPostal());
+	}
+	
+	public void instanciarCiudad_FaltaNombreCiudad_NombreCiudadIncompletaException() {
+		//arrange, act y assert
+		 Assertions.assertThrows(NombreCiudadIncompletaException.class, ()->{
+			Ciudad unaCiudad = new Ciudad(1, "", "5360");
+		});
+		
+	}
+	
+	public void instanciarCiudad_FaltaIdCiudad_IdCiudadIncompletaException() {
+		//arrange, act y assert
+		 Assertions.assertThrows(IdCiudadIncompletaException.class, ()->{
+			Ciudad unaCiudad = new Ciudad("", "5360");
+		});
+	}
+
+}
