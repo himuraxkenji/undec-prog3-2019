cff2c68b65c34a6d6798ee51dfb40b544135e68b
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-05-09 20:58:49 -0300

    Se agregaron test a aerolinea

--- aeropuerto/src/test/java/aeropuerto/AerolineaUnitTest.java
+++ aeropuerto/src/test/java/aeropuerto/AerolineaUnitTest.java
@@ -9,21 +9,27 @@ class AerolineaUnitTest {
 
 	@Test
 	public void instanciarAerolinea_TodosLosAtributos_InstanciaCorrecta() {
-		//arrange y act
 		Aerolinea unaAerolinea = new Aerolinea(1, "Flybondy");
 		
-		//assert
 		assertEquals(1, unaAerolinea.getId());
 		assertEquals("Flybondy", unaAerolinea.getNombre());
 	}
 	
 	@Test
 	public void instanciarAerolinea_FaltaNombre_InstanciaIncorrecta() {
-		//arrange y act
-		//assert
 		Assertions.assertThrows(IdAerolineaIncompletaException.class, ()->{
 			Aerolinea unaAerolinea = new Aerolinea(1,"eze1234");
 		});
+		
+	}
+	
+	@Test
+	public void ModificarAerolineaNombre_TodosLosAtributos_InstanciaCorrecta() {
+		Aerolinea unaAerolinea = new Aerolinea(1, "Flybondy");
+		
+		unaAerolinea.setNombre("AerolineasArgentinas");
+		
+		assertEquals("AerolineasArgentinas", unaAerolinea.getNombre());
 		
 	}
 
