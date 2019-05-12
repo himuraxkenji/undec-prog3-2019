c62f16a36267e3ffd27d97c5c5beb66e0c0558e1
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-05-12 20:43:57 -0300

    Se corrigio excepciones en el test Aeropuerto

--- aeropuerto/src/test/java/aeropuerto/AeropuertoUnitTest.java
+++ aeropuerto/src/test/java/aeropuerto/AeropuertoUnitTest.java
@@ -1,37 +1,72 @@
 package aeropuerto;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import excepciones.AerolineaMalFormadaException;
+import excepciones.AeropuertoCiudadIncompletaException;
+import excepciones.AeropuertoIdIncompletoException;
+import excepciones.AeropuertoNombreIncompletoException;
+import excepciones.CiudadMalFormadaException;
+
 class AeropuertoUnitTest {
 
 	@Test
 	public void instanciarAeropuerto_TodosLosAtributos_InstanciaCorrecta() {
-		Ciudad  unaCiudad = new Ciudad(1, "Buenos Aires");
-		Aeropuerto unAeropuerto = new Aeropuerto(1, "Ezeiza", unaCiudad, "eze1234");
+		Ciudad unaCiudad = null;
+		try {
+			unaCiudad = new Ciudad(1, "Buenos Aires");
+		} catch (CiudadMalFormadaException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Aeropuerto unAeropuerto = null;
+		try {
+			unAeropuerto = new Aeropuerto(1, "Ezeiza", unaCiudad, "eze1234");
+		} catch (AerolineaMalFormadaException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		assertEquals(1, unAeropuerto.getId());
 		assertEquals("Ezeiza", unAeropuerto.getNombre());
 		assertEquals(unaCiudad, unAeropuerto.getCiudad());
-		assertEquals("eze1234", unAeropuerto.getCodigo());	
+		assertEquals("eze1234", unAeropuerto.getCodigoAeropuerto());	
 	}
 	
 	@Test
 	public void instanciarAeropuerto_FaltaIdAeropuerto_IdCiudadIncompletoException() {
-		Ciudad  unaCiudad = new Ciudad(1, "Buenos Aires");
 		
-		Assertions.assertThrows(IdAeropuertoIncompletoException.class, ()->{
-			Aeropuerto unAeropuerto = new Aeropuerto("Ezeiza", unaCiudad, "eze1234");
+		Assertions.assertThrows(AeropuertoIdIncompletoException.class, ()->{
+			Ciudad unaCiudad = null;
+			try {
+				unaCiudad = new Ciudad(1, "Buenos Aires");
+			} catch (CiudadMalFormadaException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Aeropuerto unAeropuerto = new Aeropuerto(0,"Ezeiza", unaCiudad, "eze1234");
 		});
 		
 	}
 	
 	@Test
 	public void instanciarAeropuerto_FaltaNombreAeropuerto_NombreAeropuertoIncompletoException() {
-		Ciudad  unaCiudad = new Ciudad(1, "Buenos Aires");
-		Assertions.assertThrows(NombreAeropuertoIncompletoException.class, ()->{
+		Assertions.assertThrows(AeropuertoNombreIncompletoException.class, ()->{
+			Ciudad unaCiudad = null;
+			try {
+				unaCiudad = new Ciudad(1, "Buenos Aires");
+			} catch (CiudadMalFormadaException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			Aeropuerto unAeropuerto = new Aeropuerto(1, "", unaCiudad, "eze1234");
 		});
 		
@@ -39,17 +74,30 @@ class AeropuertoUnitTest {
 	
 	@Test
 	public void instanciarAeropuerto_FaltaCiudadAeropuerto_CiudadIncompletaException() {
-		Ciudad  unaCiudad = new Ciudad(1, "Buenos Aires");
-		Assertions.assertThrows(CiudadAeropuertoIncompletaException.class, ()->{
-			Aeropuerto unAeropuerto = new Aeropuerto(1, "", null, "eze1234");
+		Assertions.assertThrows(AeropuertoCiudadIncompletaException.class, ()->{
+			Aeropuerto unAeropuerto = new Aeropuerto(1, "Ezeiza", null, "eze1234");
 		});
 		
 	}
 	
 	@Test
 	public void modificarNombreAeropuerto_TodosLosAtributos_InstanciaCorrecta() {
-		Ciudad  unaCiudad = new Ciudad(1, "Buenos Aires");
-		Aeropuerto unAeropuerto = new Aeropuerto(1, "Ezeiza", unaCiudad, "eze1234");
+		Ciudad unaCiudad = null;
+		try {
+			unaCiudad = new Ciudad(1, "Buenos Aires");
+		} catch (CiudadMalFormadaException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Aeropuerto unAeropuerto = null;
+		try {
+			unAeropuerto = new Aeropuerto(1, "Ezeiza", unaCiudad, "eze1234");
+		} catch (AerolineaMalFormadaException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		unAeropuerto.setNombre("La Rioja");
 		
@@ -58,12 +106,25 @@ class AeropuertoUnitTest {
 	
 	@Test
 	public void modificarCodigo_TodosLosAtributos_InstanciaCorrecta() {
-		Ciudad  unaCiudad = new Ciudad(1, "Buenos Aires");
-		Aeropuerto unAeropuerto = new Aeropuerto(1, "Ezeiza", unaCiudad, "eze1234");
+		Ciudad unaCiudad = null;
+		try {
+			unaCiudad = new Ciudad(1, "Buenos Aires");
+		} catch (CiudadMalFormadaException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Aeropuerto unAeropuerto = null;
+		try {
+			unAeropuerto = new Aeropuerto(1, "Ezeiza", unaCiudad, "eze1234");
+		} catch (AerolineaMalFormadaException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		unAeropuerto.setCodigoAeropuerto("eze1994");
 		
-		unAeropuerto.setCodigo("eze1994");
-		
-		assertEquals("eze1994", unAeropuerto.getCodigo());
+		assertEquals("eze1994", unAeropuerto.getCodigoAeropuerto());
 	}
 	
 }
