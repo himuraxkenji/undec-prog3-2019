2ec8f28a9b580fca68a3778ff4082b530790f82b
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-05-12 15:51:55 -0300

    Se creo clase Aerolinea

---
+++ aeropuerto/src/main/java/aeropuerto/Aerolinea.java
@@ -0,0 +1,30 @@
+package aeropuerto;
+
+import excepciones.AerolineaMalFormadaException;
+import excepciones.AerolineaNombreIncompletoException;
+
+public class Aerolinea {
+
+	private Integer idAerolinea;
+	private String nombre;
+
+	public Aerolinea(Integer idAerolinea, String nombre) throws AerolineaMalFormadaException{
+		if(nombre.length() == 0)
+			throw new AerolineaNombreIncompletoException();
+		this.idAerolinea = idAerolinea;
+		this.nombre = nombre;
+	}
+
+	public Integer getId() {
+		return this.idAerolinea;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
