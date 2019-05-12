3069ab4771fae02e1ecdfa197c0c0d4c6d3a0ae6
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-05-12 15:23:10 -0300

    Se corrigio instancias de objetos con excepciones

--- aeropuerto/src/test/java/aeropuerto/PilotoUnitTest.java
+++ aeropuerto/src/test/java/aeropuerto/PilotoUnitTest.java
@@ -7,36 +7,54 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import excepciones.*;
+
 class PilotoUnitTest {
 
 	@Test
 	public void instanciarPiloto_TodosLosAtributos_InstanciaCorrecta() {
 
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1);
-		Piloto unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		Piloto unPiloto = null;
+		
+		try {
+			unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		}catch (PilotoMalFormadoException e){
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		assertEquals(1, unPiloto.getId());
 		assertEquals("Alvarez", unPiloto.getApellido());
-		assertEquals("Juan", unPiloto.getNombre());
-		assertEquals("38.440.761", unPiloto.getDni());
+		assertEquals("Juan", unPiloto.getNombres());
+		assertEquals("38440761", unPiloto.getDni());
 		assertEquals("2019-05-01", unPiloto.getFechaNacimiento());
 	}
 	
 	@Test
 	public void instanciarPiloto_FaltaFechaNacimiento_InstanciaCorrecta() {
 
-		Piloto unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761");
+		Piloto unPiloto = null;
+		
+		try {
+			unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761");
+		}catch (PilotoMalFormadoException e){
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		assertEquals(1, unPiloto.getId());
 		assertEquals("Alvarez", unPiloto.getApellido());
-		assertEquals("Juan", unPiloto.getNombre());
-		assertEquals("38.440.761", unPiloto.getDni());
+		assertEquals("Juan", unPiloto.getNombres());
+		assertEquals("38440761", unPiloto.getDni());
 	}
 	
 	@Test
 	public void instanciarPiloto_FaltaApellidoPiloto_ApellidoPilotoIncompletoException() {
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1); 
-		Assertions.assertThrows(ApellidoPilotoIncompletoException.class, ()->{
+		Assertions.assertThrows(PilotoApellidoIncompletoException.class, ()->{
 			 Piloto unPiloto = new Piloto(1, "", "Juan", "38440761", fechaNacimientoPiloto);
 		});
 		
@@ -45,7 +63,7 @@ class PilotoUnitTest {
 	@Test
 	public void instanciarPiloto_FaltaNombrePiloto_NombrePilotoIncompletoException() {
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1); 
-		Assertions.assertThrows(NombrePilotoIncompletoException.class, ()->{
+		Assertions.assertThrows(PilotoNombreIncompletoException.class, ()->{
 			 Piloto unPiloto = new Piloto(1, "Alvarez", "", "38440761", fechaNacimientoPiloto);
 		});
 		
@@ -54,7 +72,7 @@ class PilotoUnitTest {
 	@Test
 	public void instanciarPiloto_FaltaDNIPiloto_DNIPilotoIncompletoException() {
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1); 
-		Assertions.assertThrows(DNIPilotoIncompletoException.class, ()->{
+		Assertions.assertThrows(PilotoDNIIncompletoException.class, ()->{
 			 Piloto unPiloto = new Piloto(1, "Alvarez", "Juan", "", fechaNacimientoPiloto);
 		});
 		
@@ -63,8 +81,15 @@ class PilotoUnitTest {
 	@Test
 	public void modificarApellido_TodosLosDatos_ModificacionCorrecta() {
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1);
-		Piloto unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		Piloto unPiloto = null;
 		
+		try {
+			unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		}catch (PilotoMalFormadoException e){
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		unPiloto.setApellido("Lopez");
 		
 		assertEquals("Lopez", unPiloto.getApellido());
@@ -74,37 +99,56 @@ class PilotoUnitTest {
 	@Test
 	public void modificarNombre_TodosLosDatos_ModificacionCorrecta() {
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1);
-		Piloto unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		Piloto unPiloto = null;
 		
-		unPiloto.setNombre("Jose");
+		try {
+			unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		}catch (PilotoMalFormadoException e){
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		unPiloto.setNombres("Jose");
 		
-		assertEquals("Jose", unPiloto.getNombre());
+		assertEquals("Jose", unPiloto.getNombres());
 		
 	}
 	
 	@Test
 	public void modificarDNI_TodosLosDatos_ModificacionCorrecta() {
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1);
-		Piloto unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		Piloto unPiloto = null;
 		
+		try {
+			unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		}catch (PilotoMalFormadoException e){
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		unPiloto.setDNI("39774621");
 		
-		assertEquals("39774621", unPiloto.getNombre());
+		assertEquals("39774621", unPiloto.getDni());
 		
 	}
 	
 	@Test
 	public void modificarFechaNacimiento_TodosLosDatos_ModificacionCorrecta() {
 		LocalDate fechaNacimientoPiloto = LocalDate.of(2019, 5, 1);
-		Piloto unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		Piloto unPiloto = null;
 		
+		try {
+			unPiloto = new Piloto(1, "Alvarez", "Juan", "38440761", fechaNacimientoPiloto);
+		}catch (PilotoMalFormadoException e){
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 		fechaNacimientoPiloto = LocalDate.of(2019, 6, 2);
 		
-		unPiloto.setDNI(fechaNacimientoPiloto);
+		unPiloto.setFechaNacimiento(fechaNacimientoPiloto);
 		
-		assertEquals("2019-06-02", unPiloto.getNombre());
+		assertEquals("2019-06-02", unPiloto.getFechaNacimiento());
 		
 	}
-	
-
 }
