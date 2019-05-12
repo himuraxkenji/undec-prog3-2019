95d0749b1bef78478bf90e988ab91cc6b2ed81e6
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-05-12 02:54:17 -0300

    Se creo la clase Ciudad con los metodos solicitados en el test

---
+++ aeropuerto/src/main/java/aeropuerto/Ciudad.java
@@ -0,0 +1,45 @@
+package aeropuerto;
+
+import excepciones.CiudadMalFormadaException;
+import excepciones.IdCiudadIncompletaException;
+import excepciones.NombreCiudadIncompletaException;
+
+public class Ciudad {
+	
+	private Integer idCiudad;
+	private String nombre;
+	private String codigoPostal;
+
+	public Ciudad(Integer idCiudad, String nombre, String codigoPostal) throws CiudadMalFormadaException {
+		if(nombre.length() == 0)
+			throw new NombreCiudadIncompletaException();
+		if(idCiudad == 0)
+			throw new IdCiudadIncompletaException();
+		this.idCiudad = idCiudad;
+		this.nombre = nombre;
+		this.codigoPostal = codigoPostal;
+	}
+
+	public Integer getIdCiudad() {
+		return this.idCiudad;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getCodigoPostal() {
+		return this.codigoPostal;
+	}
+
+	public void setCiudad(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+	
+	
+
+}
